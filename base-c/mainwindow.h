a20aea5a52741f52e237614bd181861b97eba849
everk <757503218@qq.com>
2019-08-30 09:38:11 +0800

    version-0.1

--- base-c/mainwindow.h
+++ base-c/mainwindow.h
@@ -15,6 +15,43 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionSave_As_triggered();
+
+    void on_actionCreate_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionClose_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionSelect_All_triggered();
+
+    void on_actionAnnotation_triggered();
+
+    void on_actionUnannotation_triggered();
+
+    void on_actionRun_triggered();
+
+    void on_actionCompile_triggered();
+
+    void on_actionRun_Compile_triggered();
+
+    void on_actionStop_triggered();
+
+    void on_actionAbout_Base_C_triggered();
+
+    void on_actionMotto_of_the_day_triggered();
+
+    void on_actionAbout_us_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
